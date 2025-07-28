06ab49d9a2cf705b605e16039820b2021163da2a
Daniel Soares <daniel.soares@fortics.com.br>
2025-07-28 12:07:16 -0300

    feature: fix site header@

--- src/components/custom/SiteHeader.tsx
+++ src/components/custom/SiteHeader.tsx
@@ -3,7 +3,7 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Bell } from "lucide-react";
 
-export function SiteHeader({ activeTitle }: { activeTitle: string }) {
+export function SiteHeader({ activeTitle }: { activeTitle?: string }) {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
