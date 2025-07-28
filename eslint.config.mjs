c396cd20e059e127a9d630bad3b2eaf71ea2bcdf
Daniel Soares <daniel.soares@fortics.com.br>
2025-07-28 11:54:08 -0300

    feature: disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Disable all ESLint rules by adding an override with "rules": {}.
+  {
+    rules: {},
+  },
 ];
 
 export default eslintConfig;
